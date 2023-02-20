418f45f2f2c6295d2b996f90fb7d3d0dc52bbb2f
Ritesh Bakare <99970218+RiteshBakare@users.noreply.github.com>
2023-02-20 12:47:54 +0530

    First Commit

---
+++ app/src/main/java/com/gmail/hackdroid/visual/learn/data/structure/basicSyntax/BasicSyntaxInfo.kt
@@ -0,0 +1,17 @@
+package com.gmail.hackdroid.visual.learn.data.structure.basicSyntax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gmail.hackdroid.visual.learn.data.structure.R
+
+class BasicSyntaxInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic_syntax_info)
+
+
+    }
+
+
+
+}
